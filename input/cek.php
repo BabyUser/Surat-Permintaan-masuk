395d0d5a5bb734a2ce9bee3546d1044ce032001b
BabyUser <langlangbolang86@gmail.com>
2017-06-16 15:33:49 +0700

    Add files via upload

--- input/cek.php
+++ input/cek.php
@@ -29,40 +29,67 @@
 			<div class="row">
 				<div class="col s12">
 					<ul class="tabs">
-				        <li class="tab col s6"><a class="active" href="#test1">Test 1</a></li>
-				        <li class="tab col s6"><a href="#test2">Test 2</a></li>
+				        <li class="tab col s6"><a class="active" href="#test1">company</a></li>
+				        <li class="tab col s6"><a href="#test2">Produks</a></li>
 				    </ul>
 				</div>
-				<div id="test1" class="col s12">Test 1</div>
-    			<div id="test2" class="col s12">Test 2</div>
-			</div>
-			<table class="highlight">
-				<thead>
-					<tr>
-						<th>Id Perusahaan</th>
-						<th>Nama Perusahaan</th>
-						<th>Sertifikat</th>
-						<th>Expired Sertifikat</th>
-					</tr>
-				</thead>
-				<tbody>
-				<?php 
-				include "koneksi/conn.php";
+				<div id="test1" class="col s12">
+					<table class="highlight responsive-table centered">
+						<thead>
+							<tr>
+								<th>Id Perusahaan</th>
+								<th>Nama Perusahaan</th>
+								<th>Sertifikat</th>
+								<th>Expired Sertifikat</th>
+							</tr>
+						</thead>
+						<tbody>
+						<?php 
+						include "koneksi/conn.php";
 
-				$sql = $db->prepare("SELECT * FROM perusahaan");
-				$sql->execute();
-				while ($fet = $sql->fetch(PDO::FETCH_ASSOC)) { ?>
-					<tr>
-						<td><?php echo $fet['id_perusahaan'] ?></td>
-						<td><?php echo $fet['nama_perusahaan'] ?></td>
-						<td><?php echo $fet['sertifikat'] ?></td>
-						<td><?php echo $fet['expired_sertifikat'] ?></td>
-					</tr>
-					<?php
-				}
-				 ?>
-				</tbody>
-			</table>
+						$sql = $db->prepare("SELECT * FROM perusahaan");
+						$sql->execute();
+						while ($fet = $sql->fetch(PDO::FETCH_ASSOC)) { ?>
+							<tr>
+								<td><?php echo $fet['id_perusahaan'] ?></td>
+								<td><?php echo $fet['nama_perusahaan'] ?></td>
+								<td><?php echo $fet['sertifikat'] ?></td>
+								<td><?php echo $fet['expired_sertifikat'] ?></td>
+							</tr>
+							<?php
+						}
+						 ?>
+						</tbody>
+					</table>
+				</div>
+    			<div id="test2" class="col s12">
+    				<table class="highlight">
+						<thead>
+							<tr>
+								<th>Id Produk</th>
+								<th>Id perusahaan</th>
+								<th>Produk</th>
+							</tr>
+						</thead>
+						<tbody>
+						<?php 
+						include "koneksi/conn.php";
+
+						$sql = $db->prepare("SELECT * FROM produks");
+						$sql->execute();
+						while ($fet = $sql->fetch(PDO::FETCH_ASSOC)) { ?>
+							<tr>
+								<td><?php echo $fet['id_produk'] ?></td>
+								<td><?php echo $fet['id_perusahaan'] ?></td>
+								<td><?php echo $fet['produk'] ?></td>
+							</tr>
+							<?php
+						}
+						 ?>
+						</tbody>
+					</table>
+    			</div>
+			</div>
 		</div>
 	</div>
 
